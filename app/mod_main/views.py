a8c2f83e611204eff9a5c8e3a59f29bb57bfe782
diamanthaxhimusa <diamanthaxhimusa@gmail.com>
2017-05-02 16:32:34 +0200

    search page v1.0

--- app/mod_main/views.py
+++ app/mod_main/views.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template, json, jsonify, Response
+from flask import Blueprint, render_template, json, jsonify, Response, request, redirect
 from bson import json_util, ObjectId
 import re
+from app import mongo
 
 import sys
 reload(sys)
@@ -10,11 +11,17 @@ sys.setdefaultencoding('utf-8')
 
 mod_main = Blueprint('main', __name__)
 
-from app import mongo
 
-@mod_main.route('/')
+@mod_main.route('/', methods=['GET','POST'])
 def index():
+	db = mongo.db.reg_businesses
+	db.create_index("formatted.slugifiedOwners",1)
+	if request.method == 'POST':
+		keyword = request.form['search']
+		result = db.find({"slugifiedOwners": {"$regex": keyword}}, {'owners':1,'name': 1, 'status': 1, 'arbkUrl': 1})
+		return render_template('index.html', result=result)
 	return render_template('index.html')
+
 
 @mod_main.route('/visualization')
 def visualization():
