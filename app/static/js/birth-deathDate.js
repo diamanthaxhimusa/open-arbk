32cc293ddb774d99d5a9bf4a2d4178459c4193ff
diamanthaxhimusa <diamanthaxhimusa@gmail.com>
2017-05-29 16:14:57 +0200

    minor improvements

--- app/static/js/birth-deathDate.js
+++ app/static/js/birth-deathDate.js
@@ -14,11 +14,6 @@ $(document).ready(function(){
             title: {
                 text: 'Krijimi/Mbyllja e bizneseve sipas viteve 2002-2018'
             },
-            legend: {
-                layout: 'vertical',
-                align: 'right',
-                verticalAlign: 'middle'
-            },
             xAxis: {
                 categories: ['2002', '2003', '2004', '2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017']
             },
